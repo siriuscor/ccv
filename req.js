67473cdead56baaf3031934a566c059bd86f8dba
Li Yi <siriuscor@gmail.com>
2016-05-25 23:23:11 +0800

    test

--- req.js
+++ req.js
@@ -1,24 +1,29 @@
 var url = require('url');
 var http = require('http');
+var https = require('https');
 
 var cache = {};
-function get(_url, callback) {
-    // var postData = querystring.stringify({
-    //   'msg' : 'Hello World!'
-    // });
-    if (cache[_url]) {
-      callback(cache[_url]);
-      return;
-    }
+var retry_map = {};
+
+var MAX_RETRY = 10;
+
+function get(_url, extra_header, callback) {
+    // if (cache[_url]) {
+    //   callback(cache[_url]);
+    //   return;
+    // }
 
     console.log("request:" + _url);
     var options = url.parse(_url);
-    options.headers = {
+    options.headers = extra_header;
+    // options.headers = {
         // 'referer': 'http://ac.qq.com/'
         // 'referer' : 'http://manhua.dmzj.com/'
-    };
+        // 'Cookie' : 'JSESSIONID=zfpkXDTDycyzsM561JlLLQh48w33TYgGLq6Z6wRdR7sSsHpV4JCB'
+    // };
 
-    var req = http.get(options, (res) => {
+    var c = options['protocol'] == 'http:' ? http : https;
+    var req = c.get(options, (res) => {
       // console.log(`STATUS: ${res.statusCode}`);
       // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
       var text = "";
@@ -30,7 +35,8 @@ function get(_url, callback) {
       res.on('end', () => {
         if (text == '') {
             console.error('request empty body');
-            callback(null);
+            // callback(null);
+            retry(_url, extra_header, callback);
             return;
         }
         cache[_url] = text;
@@ -40,17 +46,42 @@ function get(_url, callback) {
     });
 
     req.on('error', (e) => {
-      // console.log("req on error", e);
-      callback(null, e);
+      console.log("req on error", e);
+    //   callback(null, e);
+      retry(_url, extra_header, callback);
       // console.error(`problem with request: ${e.message}`);
     });
-
+    req.setTimeout(10000);
     // write data to request body
     // req.write(postData);
     // req.end();
 }
 
+function getP(_url , extra_header) {
+    return new Promise(function(resolve, reject) {
+        get(_url, extra_header, function(data) {
+            if (data == null) {
+                reject();
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function retry(_url ,extra_header, callback) {
+    if (retry_map[_url] == undefined) retry_map[_url] = 0;
+    retry_map[_url] ++;
+    // console.log(retry_map);
+    if (retry_map[_url] > MAX_RETRY) {
+        console.log('retry max reach');
+        callback(null);
+    } else {
+        get(_url, extra_header, callback);
+    }
+}
 // get('http://images.dmzj.com/y/%E9%93%B6%E6%B2%B3%E9%AA%91%E5%A3%AB%E4%BC%A0/%E7%AC%AC02%E5%8D%B7/Yhqsz02-004.jpg', console.log);
 // get('http://ac.qq.com/Comic/searchList/search/onepiece');
-// get('http://www.baidu.com', console.log);
+// get('http://www.baid.cm', {}, console.log);
 exports.get = get;
+exports.getP = getP;
--- test.js
+++ test.js
@@ -16,4 +16,58 @@
 //     return p
 // }('y s=s=\'["c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/x.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/A.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/w.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/t.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/u.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/v.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/z.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/I.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/G.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/H.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/F.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/B.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/E.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/C.b","c\\/%0%j%9%0%h%8%f%e%7%0%a%9%1%8%d%6%i%m%6%r%p%1%7%4%2%3%4%2%l%5%1%3%5%g\\/%2%o%k%0%n%q\\/D.b"]\';',45,45,'E8|E9|E7|9A|84|BE|E6|9B|BD|85|80|jpg||90|8A|E5|20|83|9C|B6|AC175|81|A8|AF|AC|A0|9D|A5|pages|JOJO_004|JOJO_005|JOJO_006|JOJO_003|JOJO_001|var|JOJO_007|JOJO_002|JOJO_012|JOJO_014|JOJO_015|JOJO_013|JOJO_011|JOJO_009|JOJO_010|JOJO_008'.split('|'),0,{})
 
-(function(a){console.log(a);})('123');
+// (function(a){console.log(a);})('123');
+// 'use strict';
+// new Promise(function(resolve, reject){
+    // console.log("test");
+// })
+
+var p = Promise.resolve();
+function chain(func) {
+    p = p.then(() => {
+        return new Promise(func);
+    });
+}
+
+
+
+var times = [1000,2000,3000,4000, 1000];
+// times.each(function(i){
+//     console.log(i);
+// });
+// console.log("enter0");
+var j = 0;
+// for (var i = 0; i < times.length; i++) {
+//     chain(function(resolve, reject){
+//             // console.log("enter",i);
+//             setTimeout(() => {
+//                 // process.stdout.write("received: " + j + "\x1B[0G");
+//                 process.stdout.write('\033[0G'); process.stdout.write('newstuff' + (j++));
+//                 resolve(i);
+//             },
+//             times[i]);
+//     });
+setInterval(function() {
+        // process.stdout.write("received: " + j + "\x1B[0G");
+        process.stdout.write('\x1B[0G'); process.stdout.write('newstuff' + (j++));
+}, 1000);
+//     p = p.then(
+// // function(li){
+//         () => {
+//             return new Promise(function(resolve, reject){
+//             // var li = i;
+//             console.log("enter",i);
+//             setTimeout(() => {
+//                 console.log('finish ' + i);
+//                 resolve(i);
+//             },
+//             times[i]);
+//         })
+//     }
+// }(i)
+// )
+// }
+
+
+// p2.then(result => console.log(result))
+// p2.catch(error => console.log(error))
